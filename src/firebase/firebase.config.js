351351a97c8de6aced5e6b3ca339b6ed4ac883f7
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-26 23:53:39 +0600

    setup project structure and install some package and libeary

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASiZDbwhoOhkC9bty5xLCdVtuLAEF9JEU",
+  authDomain: "discoverwander-16ac4.firebaseapp.com",
+  projectId: "discoverwander-16ac4",
+  storageBucket: "discoverwander-16ac4.appspot.com",
+  messagingSenderId: "357310420390",
+  appId: "1:357310420390:web:649fadcf78199532e7ab67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
